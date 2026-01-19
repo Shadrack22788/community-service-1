45078bc5f795bab9ff491daa9617e09214cf2b28
Shadrack22788 <tuyizeresh@gmail.com>
2026-01-19 14:38:52 +0200

    lesson of verifyAccess

--- src/middleware/VerifyAccess.js
+++ src/middleware/VerifyAccess.js
@@ -8,7 +8,17 @@ export function VerifyAccess(passRole) {
         }else{
             try {
                 const verifyToken = jwt.verify(token, process.env.SECRET_KEYE,{expiresIn:"2d"})
+                req.user = verifyToken.user
+                if (passRole != req.user.role) {
+                    return res.status(401).json({ message: "Access denied!"})
+
+                }
             }catch(error) {
+                if ((error.name = "JsonWebTokenError")) {
+                    return res.status(403).json({ message: "Invalid token! or expired token"})
+                }else{
+                    return res.status(500).json({ message: 'error is ${error}'})
+                }
 
             }
         }
