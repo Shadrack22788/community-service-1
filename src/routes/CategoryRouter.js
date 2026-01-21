00ce84d6883745b3a98c71b422a8e6570390bc16
Shadrack22788 <tuyizeresh@gmail.com>
2026-01-21 11:37:41 +0200

    change category

--- src/routes/CategoryRouter.js
+++ src/routes/CategoryRouter.js
@@ -1,8 +1,9 @@
 import express from  "express"
 import CategoryController from "../controller/CategoryController.js"
 
-
 const router = express.Router()
 
+router.get("/create", CategoryController.createCategory)
 router.post("/category", CategoryController.createCategory)
+
 export default router
